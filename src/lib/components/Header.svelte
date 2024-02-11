2eacefe1b619151998674dcd09a3ff61994e2f5f
Ilya <iva2k@yahoo.com>
2024-02-11 07:55:03 -0800

    Add +layout.svelte and components/Header.svelte

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<!-- src/components/Header.svelte -->
+<script>
+	// Your header component logic goes here
+</script>
+
+<div class="header">
+	<!-- Your header content goes here -->
+        Hello, I'm a header
+</div>
+
+<style>
+	/* Your header styles go here */
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<div class="app">
+	<Header />
+	<main>
+		<slot />
+	</main>
+</div>
+
+<!-- <style lang="scss">
+</style> -->
+
